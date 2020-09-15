1f2b42eff3c6fa6de3c7f535471b35004f6eb700
fikri <me@fikri.tech>
2020-09-15 12:17:43 +0700

    req.params

--- sporteens-api/index.js
+++ sporteens-api/index.js
@@ -39,11 +39,9 @@ app.post('/product' , (req,res) => {
     // VALUES ("${data.name}" , "${data.price}","${data.images_1}","${data.images_2}","${data.images_3}","${data.category_id}", "${data.stock}");`
 
     // cara simple insert data , syaratnya prop object === colomn di database
-
-    
     var query_2 = 'insert into products set ?'
     db.query(query_2 ,data , (err,result) => {
-        
+
         // buat handling error
         try {
             if(err) throw err
@@ -58,6 +56,36 @@ app.post('/product' , (req,res) => {
 
 })
 
+// READ DATA
+
+app.get('/products', (req,res) => {
+    var query = 'select * from products;'
+    db.query(query,(err,result) => {
+        try {
+            if(err) throw err
+            res.send(result)
+        } catch (error) {
+            res.send(error.message)
+        }
+    })
+})
+
+
+app.get('/product/:id' , (req,res) => {
+    var id = req.params.id
+    var sqlQuery = 'select * from products where id = ?'
+    db.query(sqlQuery,id,(err,result) => {
+        try {
+            if(result.length > 0){
+                res.send(result)
+            }else{
+                res.send("Data with id " + id + " not found")
+            }
+        } catch (error) {
+            res.send(error.message)
+        }
+    }) 
+})
 
 
 app.listen(PORT , () => console.log('API RUNNING ON PORT ' + PORT))
